169009fbf87e4c16c98663aa4d8ab5aeb1ff6596
NARARYA JAYA UTAMA GROUB <nararyajayautamagroub@gmail.com>
2026-09-20 13:15:41 +0700

    types: add password capability to NextAuth session

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,9 +1,9 @@
 import "next-auth"; import "next-auth/jwt";
 declare module "next-auth" {
   interface Session {
-    user:{id:string;name?:string|null;email?:string|null;image?:string|null;role:string;locale:string}
+    user:{id:string;name?:string|null;email?:string|null;image?:string|null;role:string;locale:string;hasPassword:boolean}
   }
-  interface User {role?:string;locale?:string}
+  interface User {role?:string;locale?:string;hasPassword?:boolean}
 }
 declare module "next-auth/jwt" {
   interface JWT {uid?:string;role?:string;locale?:string}
